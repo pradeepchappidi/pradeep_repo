f1856336356028115580a46565e5c1b563618fd3
PXC060470 <PXC060470@X7001570.ctl.intranet>
2015-02-16 11:57:02 -0600

    made more changes

--- django_git_test/src/django_git_test/settings.py
+++ django_git_test/src/django_git_test/settings.py
@@ -15,6 +15,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 
+## changed again 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
